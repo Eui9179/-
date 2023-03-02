d4e5d11aa2f8800cde5b7644795778fdfa4ab76d
Eui9179 <outdark97@gmail.com>
2023-03-03 03:22:54 +0900

    init-server

---
+++ woojoo-spring-server/src/main/java/leui/woojoo/WoojooApplication.java
@@ -0,0 +1,13 @@
+package leui.woojoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoojooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoojooApplication.class, args);
+    }
+
+}
---
+++ woojoo-spring-server/src/test/java/leui/woojoo/WoojooApplicationTests.java
@@ -0,0 +1,13 @@
+package leui.woojoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoojooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
