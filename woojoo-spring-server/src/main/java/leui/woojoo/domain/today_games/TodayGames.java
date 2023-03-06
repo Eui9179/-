445314f493ed6ac2c414c7b582eda4584cc18f92
Eui9179 <outdark97@gmail.com>
2023-03-06 23:01:31 +0900

    UserGroups 저장 테스트코드 추가

---
+++ woojoo-spring-server/src/main/java/leui/woojoo/domain/today_games/TodayGames.java
@@ -0,0 +1,32 @@
+package leui.woojoo.domain.today_games;
+
+import jakarta.persistence.*;
+import leui.woojoo.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class TodayGames extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(length = 40, nullable = false)
+    private String gameName;
+
+    @Column(columnDefinition = "TEXT")
+    private String descriptions;
+
+    @Builder
+    public TodayGames(Long userId, String gameName, String descriptions) {
+        this.userId = userId;
+        this.gameName = gameName;
+        this.descriptions = descriptions;
+    }
+}
---
+++ woojoo-spring-server/src/main/java/leui/woojoo/domain/today_games/TodayGamesRepository.java
@@ -0,0 +1,6 @@
+package leui.woojoo.domain.today_games;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodayGamesRepository extends JpaRepository<TodayGames, Long> {
+}
