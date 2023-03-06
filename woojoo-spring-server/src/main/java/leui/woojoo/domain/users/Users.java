a3a5eeb68983e407aabc41dfd5ed9452439fec58
Eui9179 <outdark97@gmail.com>
2023-03-06 20:33:37 +0900

    feature/UsersRepository 저장 테스트

--- woojoo-spring-server/src/main/java/leui/woojoo/domain/users/Users.java
+++ woojoo-spring-server/src/main/java/leui/woojoo/domain/users/Users.java
@@ -2,6 +2,7 @@ package leui.woojoo.domain.users;
 
 import jakarta.persistence.*;
 import leui.woojoo.domain.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,9 +22,25 @@ public class Users extends BaseTimeEntity {
     private String phoneNumber;
 
     @Column(length = 200)
-    private String profile_image_name;
+    private String profileImageName;
 
     @Column(length = 200)
-    private String fcm_token;
+    private String fcmToken;
 
+    @Builder
+    public Users(String name, String phoneNumber, String profileImageName, String fcmToken) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.profileImageName = profileImageName;
+        this.fcmToken = fcmToken;
+    }
+
+    public void updateProfile(String name, String profileImageName) {
+        this.name = name;
+        this.profileImageName = profileImageName;
+    }
+
+    public void updateFcmToken(String fcmToken) {
+        this.fcmToken = fcmToken;
+    }
 }
