f53ad3617961938e1a7abd83a405f7e81028cf47
Eui9179 <outdark97@gmail.com>
2023-03-06 21:38:02 +0900

    feature/UserGroups테이블 추가

---
+++ woojoo-spring-server/src/main/java/leui/woojoo/domain/user_groups/UserGroups.java
@@ -0,0 +1,35 @@
+package leui.woojoo.domain.user_groups;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class UserGroups {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @Column(length = 30, nullable = false)
+    private String groupName;
+
+    @Column(length = 30, nullable = false)
+    private String detail1;
+
+    @Builder
+    public UserGroups(Long userId, String groupName, String detail1) {
+        this.userId = userId;
+        this.groupName = groupName;
+        this.detail1 = detail1;
+    }
+
+    public void update(String groupName, String detail1) {
+        this.groupName = groupName;
+        this.detail1 = detail1;
+    }
+}
---
+++ woojoo-spring-server/src/main/java/leui/woojoo/domain/user_groups/UserGroupsRepository.java
@@ -0,0 +1,6 @@
+package leui.woojoo.domain.user_groups;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserGroupsRepository extends JpaRepository<UserGroups, Long> {
+}
